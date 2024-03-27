aaebd1c9166792c2d23e25b7f3aa5b1848062a4d
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2024-03-27 22:16:19 +0100

    calculate each pixel color

--- script.js
+++ script.js
@@ -10,9 +10,32 @@ myImage.addEventListener('load', () => {
   canvas.height = 706;
   ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
   const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  console.log(pixels)
   let particlesArray = [];
   const numberOfParticles = 300;
   let mappedImage = [];
+  for (let y = 0; y < canvas.height; y++ ) {
+    let row = [];
+    for (let x = 0; x < canvas.width; x ++) { 
+      const red = pixels.data[(y * 4 * pixels.width) + (x * 4)];
+      const green = pixels.data[(y * 4 * pixels.width) + (x * 4 + 1)];
+      const blue = pixels.data[(y * 4 * pixels.width) + (x * 4 + 2)];
+      const brightness = calculateRelativeBrightness(red, green, blue);
+      const cell = [
+        cellBrightness = brightness
+      ];
+      row.push(cell);
+    }
+    mappedImage.push(row);
+  }
+  console.log(mappedImage);
+  function calculateRelativeBrightness(red, green, blue) {
+    return Math.sqrt(
+      (red * red) * 0.299 +
+      (green * green) * 0.587 +
+      (blue * blue) * 0.114 
+    ) / 100;
+  }
   class Particle { 
     constructor() {
       this.x = Math.random() * canvas.width;
@@ -20,7 +43,8 @@ myImage.addEventListener('load', () => {
       this.speed = 0;
       this.velocity = Math.random() * 3.5;
       this.size = Math.random() * 1.5 + 1;
-
+      this.position1 = Math.floor(this.y);
+      this.position2 = Math.floor(this.x);
     }
     update() {
       this.y += this.velocity;
